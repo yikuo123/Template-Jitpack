c4cdb93119116c607e8997709d54889c5d49cae7
yikuo123 <13978174+yikuo123@users.noreply.github.com>
2024-11-01 15:10:22 +0800

    完善jni日志

--- library/src/main/cpp/log.h
+++ library/src/main/cpp/log.h
@@ -4,7 +4,9 @@
 #include <jni.h>
 #include <android/log.h>
 
+#ifndef LOG_TAG
 #define LOG_TAG "JNI"
+#endif
 
 #define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
