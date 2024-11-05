ca9531eb08bef18aa4a15f948084bc1cf46f0796
yikuo123 <13978174+yikuo123@users.noreply.github.com>
2024-11-05 16:31:26 +0800

    优化demo

--- app/src/main/java/com/ikecin/demo/MainActivity.java
+++ app/src/main/java/com/ikecin/demo/MainActivity.java
@@ -4,18 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.ikecin.demo.databinding.ActivityMainBinding;
 import com.ikecin.sdk.jni.Demo;
 
 public class MainActivity extends Activity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        findViewById(R.id.ok).setOnClickListener(v -> {
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.ok.setOnClickListener(v -> {
             Toast.makeText(getApplicationContext(), Demo.text(), Toast.LENGTH_SHORT).show();
         });
     }
-
 }
